49a74db7660723a5f5c10f3e73cb034e576738e1
David Itkin <ditkin@hubspot.com>
2021-12-31 14:20:09 -0500

    Set up node server

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema, GraphQLSchema } from 'graphql';
+import cors from 'cors';
+
+// Construct a schema, using GraphQL schema language
+const schema: GraphQLSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
